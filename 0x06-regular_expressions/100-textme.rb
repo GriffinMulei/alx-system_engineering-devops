f89872342cbff0f57d8eff2f5e54f454915aa5af
GriffinMulei <muleigriffin@gmail.com>
2023-08-01 11:33:12 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-ruby textme_statistics.rb "[from:SENDER] [to:RECEIVER] [flags:FLAGS]"
+output = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+output.each do |match|
+  sender = match[0]
+  receiver = match[1]
+  flags = match[2]
+  puts "#{sender},#{receiver},#{flags}"
+end
